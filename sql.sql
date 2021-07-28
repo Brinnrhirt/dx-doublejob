0cb57a48aa62ed3d8ea29662d4b7fad01772cbfc
Brinnrhirt <65202761+Brinnrhirt@users.noreply.github.com>
2021-07-28 13:50:40 -0500

    added a unemployed2 for double salary

--- sql.sql
+++ sql.sql
@@ -1,2 +1,8 @@
-ALTER TABLE users ADD secondjob VARCHAR(32) NOT NULL DEFAULT 'unemployed';
-ALTER TABLE users ADD secondjob_grade VARCHAR(32) NOT NULL DEFAULT '0';
+ALTER TABLE users ADD job2 VARCHAR(32) NOT NULL DEFAULT 'unemployed2';
+ALTER TABLE users ADD job2_grade VARCHAR(32) NOT NULL DEFAULT '0';
+INSERT INTO `jobs` (name, label) VALUES
+	('unemployed2','Citizen')
+;
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('unemployed2',0,'unemployed2','Unemployed',20,'{}','{}')
+;
